d88ff740dd79a74458f5a32ccc4c0d750d2889df
kurultai <vivianzhu1211@gmail.com>
2024-07-21 22:52:00 -0400

    add get place details frontend mapping

--- MealMates-api/src/main/kotlin/com/backend/mealmatesapi/constants/constants.kt
+++ MealMates-api/src/main/kotlin/com/backend/mealmatesapi/constants/constants.kt
@@ -3,5 +3,5 @@ package com.backend.mealmatesapi.constants
 object FieldMasks {
     // depending on our needs we may have to add more to this
     val SEARCH_NEARBY_DEFAULT = "places.id,places.displayName,places.shortFormattedAddress,places.location,places.photos,places.types,places.rating"
-    val PLACE_DETAILS_DEFAULT = "id,displayName,types,photos,adrFormatAddress,currentOpeningHours,priceLevel,rating,websiteUri"
+    val PLACE_DETAILS_DEFAULT = "id,displayName,types,photos,adrFormatAddress,location,currentOpeningHours,priceLevel,rating,websiteUri"
 }
---
+++ MealMates/app/src/main/java/com/example/mealmates/models/GetPlaceDetailsResponse.kt
@@ -0,0 +1,53 @@
+package com.example.mealmates.models
+
+import com.google.android.gms.maps.model.LatLng
+import com.google.gson.JsonParser
+
+class GetPlaceDetailsResponse(private var responseString: String) {
+    fun getMealMatesPlace(): MealMatesPlace {
+        try {
+            val placeJSONObject = JsonParser.parseString(responseString).asJsonObject
+                ?: throw Exception("invalid/empty GetPlaceDetailsResponse: empty `placeJSON` object")
+            val id = placeJSONObject["id"].asString
+            val types = placeJSONObject["types"].asJsonArray
+            val typesListOfStrings = mutableListOf<String>()
+            for (type in types) {
+                typesListOfStrings.add(type.asString)
+            }
+            val shortFormattedAddress = placeJSONObject["adrFormatAddress"].asString
+
+            val locationJSONObject = placeJSONObject["location"].asJsonObject
+            val location =
+                LatLng(
+                    locationJSONObject["latitude"].asDouble,
+                    locationJSONObject["longitude"].asDouble)
+
+            val rating =
+                if (placeJSONObject["rating"] != null) placeJSONObject["rating"].asDouble
+                else 0.0
+
+            val displayName = placeJSONObject["displayName"].asJsonObject["text"].asString
+
+            val photosList = mutableListOf<MealMatesPhoto>()
+            if (placeJSONObject["photos"] != null) {
+                for (photo in placeJSONObject["photos"].asJsonArray) {
+                    val photoJSONObject = photo.asJsonObject
+                    val widthPx = photoJSONObject["widthPx"].asInt
+                    val heightPx = photoJSONObject["heightPx"].asInt
+                    val photoReference = photoJSONObject["name"].asString
+                    photosList.add(MealMatesPhoto(widthPx, heightPx, photoReference))
+                }
+            }
+            return MealMatesPlace(
+                id,
+                typesListOfStrings,
+                shortFormattedAddress,
+                location,
+                rating,
+                displayName,
+                photosList)
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+}
